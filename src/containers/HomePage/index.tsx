9144f23033b7baebff641d31fc4025ce94e84191
Marcelo Santos <rocha.mer21@gmail.com>
2020-12-25 21:50:29 -0300

    att

--- src/containers/HomePage/index.tsx
+++ src/containers/HomePage/index.tsx
@@ -1,4 +1,5 @@
 import { Header } from '../../components/Header'
+import { MainContainer } from '../../components/MainContainer'
 import { PostData } from '../../domain/posts/post'
 import { Container } from './style'
 
@@ -10,14 +11,15 @@ export default function HomePage({ posts }: HomePageProps) {
   return (
     <>
       <Header />
-      <Container>
-        {posts.map((post) => (
-          <>
-            <img src={post.cover.url} alt="" />
-            <h2 key={post.slug}>{post.title}</h2>
-          </>
-        ))}
-      </Container>
+      <MainContainer>
+        <Container>
+          {posts.map((post) => (
+            <>
+              <h2 key={post.slug}>{post.title}</h2>
+            </>
+          ))}
+        </Container>
+      </MainContainer>
     </>
   )
 }
