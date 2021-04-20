8c41458934eb940f99afb9caca5aa4dcf7f20e22
Marcelo Santos <rocha.mer21@gmail.com>
2021-04-19 23:22:25 -0300

    dando inicio no stilo do component PostCard

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -1,11 +1,27 @@
 import Link from 'next/link'
 import { Container, PostCardCover, PostCardHeading } from './styled'
 
-export const PostCard = () => {
+export type PostCardProps = {
+  slug: string
+  title: string
+  cover: string
+}
+
+export const PostCard = ({ slug, title, cover }: PostCardProps) => {
   return (
     <Container>
-      <PostCardCover></PostCardCover>
-      <PostCardHeading></PostCardHeading>
+      <PostCardCover>
+        <Link href="/post/[slug]" as={`/post/[slug]`}>
+          <a>
+            <img src={cover} alt={title} />
+          </a>
+        </Link>
+      </PostCardCover>
+      <PostCardHeading>
+        <Link href="/post/[slug]" as={`/post/[slug]`}>
+          <a>{title}</a>
+        </Link>
+      </PostCardHeading>
     </Container>
   )
 }
--- src/containers/HomePage/index.tsx
+++ src/containers/HomePage/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from '../../components/Header'
 import { MainContainer } from '../../components/MainContainer'
+import { PostCard } from '../../components/PostCard'
 import { PostData } from '../../domain/posts/post'
 import { Container } from './style'
 
@@ -15,7 +16,12 @@ export default function HomePage({ posts }: HomePageProps) {
         <Container>
           {posts.map((post) => (
             <>
-              <h2 key={post.slug}>{post.title}</h2>
+              <PostCard
+                key={post.slug}
+                cover={post.cover.formats.small.url}
+                slug={post.slug}
+                title={post.title}
+              />
             </>
           ))}
         </Container>
