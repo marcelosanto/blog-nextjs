a911c56ff0bf976767896acd1e4666748fe5c923
Marcelo Santos <rocha.mer21@gmail.com>
2021-05-24 14:44:24 -0300

    ajustes no.env

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -2,4 +2,4 @@ export const API_URL = 'https://obscure-mesa-38003.herokuapp.com'
 export const POSTS_URL = `${API_URL}/posts`
 
 export const SITE_NAME = 'Marcelo Santos'
-export const SITE_URL = ''
+export const SITE_URL = process.env.SITE_URL || 'http://127.0.0.1/3000'
